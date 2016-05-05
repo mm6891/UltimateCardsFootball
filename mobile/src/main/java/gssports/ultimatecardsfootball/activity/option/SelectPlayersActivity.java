bd963a5321fc16b3e2e73235194fb3233b58d791
mm6891 <manuel.molero@gmail.com>
2016-05-05 12:37:32 +0200

    05052016

--- mobile/src/main/java/gssports/ultimatecardsfootball/activity/option/SelectPlayersActivity.java
+++ mobile/src/main/java/gssports/ultimatecardsfootball/activity/option/SelectPlayersActivity.java
@@ -28,30 +28,29 @@ public class SelectPlayersActivity extends Activity {
 		
 	public static final String TAG = "SelectPlayersActivity";			
 		
-    //botonera terreno de juego
-    private ImageButton btn00;
+    //botonera terreno de juego   
     private GridLayout gvStadiumSide;
+	
 	//boton portero
-	private Button imgBtn20;
-		
+	private Button imgBtn20;		
 	//botones defensas
 	private Button imgBtn00;
 	private Button imgBtn10;
-	//private Button imgBtn20;
+	private Button imgBtn21;
 	private Button imgBtn30;
 	private Button imgBtn40;
 	
 	//botones medios
 	private Button imgBtn01;
 	private Button imgBtn11;
-	private Button imgBtn21;
+	private Button imgBtn22;
 	private Button imgBtn31;
 	private Button imgBtn41;
 	
 	//botones delanteros
 	private Button imgBtn02;
 	private Button imgBtn12;
-	private Button imgBtn22;
+	private Button imgBtn23;
 	private Button imgBtn32;
 	private Button imgBtn42;
 	
@@ -62,21 +61,22 @@ public class SelectPlayersActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.team);		
 		
-		imgBtn00 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn10 = (Button) findViewById(R.id.imgBtn20);
 		imgBtn20 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn30 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn40 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn01 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn11 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn21 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn31 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn41 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn02 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn12 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn22 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn32 = (Button) findViewById(R.id.imgBtn20);
-		imgBtn42 = (Button) findViewById(R.id.imgBtn20);
+		imgBtn00 = (Button) findViewById(R.id.imgBtn00);
+		imgBtn10 = (Button) findViewById(R.id.imgBtn10);
+		imgBtn21 = (Button) findViewById(R.id.imgBtn21);
+		imgBtn30 = (Button) findViewById(R.id.imgBtn30);
+		imgBtn40 = (Button) findViewById(R.id.imgBtn40);
+		imgBtn01 = (Button) findViewById(R.id.imgBtn01);
+		imgBtn11 = (Button) findViewById(R.id.imgBtn11);
+		imgBtn22 = (Button) findViewById(R.id.imgBtn22);
+		imgBtn31 = (Button) findViewById(R.id.imgBtn31);
+		imgBtn41 = (Button) findViewById(R.id.imgBtn41);
+		imgBtn02 = (Button) findViewById(R.id.imgBtn02);
+		imgBtn12 = (Button) findViewById(R.id.imgBtn12);
+		imgBtn23 = (Button) findViewById(R.id.imgBtn23);
+		imgBtn32 = (Button) findViewById(R.id.imgBtn32);
+		imgBtn42 = (Button) findViewById(R.id.imgBtn42);
 		
 		btnDone = (Button) findViewById(R.id.btnDone);
 		
